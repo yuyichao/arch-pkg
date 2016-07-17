dfec070216fb272512486d0c348955044d6d142b
Yichao Yu <yyc1992@gmail.com>
2016-07-17 09:39:30 -0400

    Move more to shell script

--- compile_package.sh
+++ compile_package.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 pkg=$1
+arch=$2
 
+cachedir=$PWD/cache.$arch/$pkg
 export PKGDEST=$PWD/pkgdir
 export MAKEFLAGS="-j2"
+
+# Setup keys
+sudo pacman-key --init
+sudo pacman-key -r 4209170B
+sudo pacman-key --lsign-key 4209170B
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate archlinuxcn
+
+# Setup package dir
+rm -rf pkgdir
+mkdir pkgdir
+mkdir -p "$cachedir"
+git clone git://github.com/archlinuxcn/repo archlinuxcn
+
+# Build package
 cd archlinuxcn/$pkg && makepkg -s --noconfirm
