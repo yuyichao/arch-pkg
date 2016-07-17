e5af03be1ac4b894284b21c586633c39507d41b8
Yichao Yu <yyc1992@gmail.com>
2016-07-17 10:38:19 -0400

    Try to speed up skipping build

--- compile_package.sh
+++ compile_package.sh
@@ -9,18 +9,11 @@ cachedir=$PWD/cache.$arch/$pkg
 export PKGDEST=$PWD/pkgdir
 export MAKEFLAGS="-j2"
 
-# Setup keys
-sudo pacman-key --init
-sudo pacman-key -r 4209170B
-sudo pacman-key --lsign-key 4209170B
-sudo pacman-key --populate archlinux
-sudo pacman-key --populate archlinuxcn
-
 # Setup package dir
 rm -rf pkgdir
 mkdir pkgdir
 mkdir -p "$cachedir"
-git clone git://github.com/archlinuxcn/repo archlinuxcn
+git clone --depth 1 git://github.com/archlinuxcn/repo archlinuxcn
 
 # Build package
 cd archlinuxcn/$pkg
@@ -28,7 +21,7 @@ get_pkgver() {
     (. PKGBUILD; echo $pkgver)
 }
 # Update version first
-makepkg -s -o --noconfirm
+makepkg -o -d --noconfirm
 if [[ -f $cachedir/version ]]; then
     oldver=$(cat "$cachedir/version")
     if [[ $(get_pkgver) = $oldver ]]; then
@@ -36,5 +29,13 @@ if [[ -f $cachedir/version ]]; then
         exit 0
     fi
 fi
+
+# Setup keys
+sudo pacman-key --init
+sudo pacman-key -r 4209170B
+sudo pacman-key --lsign-key 4209170B
+sudo pacman-key --populate archlinux
+sudo pacman-key --populate archlinuxcn
+
 makepkg -s -e --noconfirm
 get_pkgver > $cachedir/version
